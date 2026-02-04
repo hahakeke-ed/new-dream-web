ae3127656f3329e828620b8be7d6e567022b04f2
hahakeke-ed <i25263698@gmail.com>
2026-02-04 15:21:22 +0900

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,12 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  // 빌드 중 ESLint 오류가 있어도 실패하지 않게
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
-  // 빌드 중 TS 오류가 있어도 실패하지 않게
+  // 빌드 중 발생할 수 있는 오류를 무시하여 배포를 우선 진행합니다.
   typescript: {
     ignoreBuildErrors: true,
+  },
+  eslint: {
+    // 최신 버전의 Next.js에서는 ignoreDuringBuilds 설정을 이렇게 감싸야 합니다.
+    ignoreDuringBuilds: true,
   },
 };
 
